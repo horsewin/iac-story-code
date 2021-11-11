5f2397a8c2906825860b1ada5e03ec308c1ad621
horsewin <mamamatz@gmail.com>
2021-11-11 16:22:51 +0900

    chore: echo prometheus and xray

--- app/go.mod
+++ app/go.mod
@@ -3,7 +3,18 @@ module github.com/uma-arai/iac-story-code/app
 go 1.16
 
 require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/aws/aws-sdk-go v1.42.2 // indirect
+	github.com/aws/aws-xray-sdk-go v1.6.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/labstack/echo-contrib v0.11.0
 	github.com/labstack/echo/v4 v4.3.0
+	github.com/valyala/fasthttp v1.31.0 // indirect
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/net v0.0.0-20211109214657-ef0fda0de508 // indirect
+	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211104193956-4c6863e31247 // indirect
+	google.golang.org/grpc v1.42.0 // indirect
 )
--- app/infrastructure/router.go
+++ app/infrastructure/router.go
@@ -1,6 +1,11 @@
 package infrastructure
 
 import (
+	"net/http"
+	"strings"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/labstack/echo-contrib/prometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/uma-arai/iac-story-code/app/handler"
@@ -14,6 +19,13 @@ func Router() *echo.Echo {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Xray middleware
+	e.Use(XrWithSkipper())
+
+	// Prometheus middleware
+	p := prometheus.NewPrometheus("Echo", nil)
+	p.Use(e)
+
 	basePath := "cnis"
 
 	AppHandler := handler.NewAppHandler(NewSQLHandler())
@@ -25,4 +37,31 @@ func Router() *echo.Echo {
 	e.GET(basePath+"/v1/app", AppHandler.GetAppInfo())
 	e.GET(basePath+"/v1/param", AppHandler.GetParamInfo())
 	return e
+}
+
+// contains... check if a string is present in a slice
+func contains(str string, arr []string) bool {
+	for _, v := range arr {
+		if strings.Contains(str, v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// XrWithSkipper ... Setup AWS X-ray middleware for skipping some URL request
+func XrWithSkipper() echo.MiddlewareFunc {
+	return echo.WrapMiddleware(func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			targets := []string{"/healthcheck"}
+			var next http.Handler
+			if contains(r.URL.Path, targets) {
+				next = h
+			} else {
+				next = xray.Handler(xray.NewFixedSegmentNamer("inbound-api"), h)
+			}
+			next.ServeHTTP(w, r)
+		})
+	})
 }
